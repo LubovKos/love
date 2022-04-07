2cda147becbcbb8f120e4ff34165500ec7ae9f44
email <maxim.dementev.04@gmail.com>
2022-04-07 23:36:41 +0300

    Поменял файлы

--- matrix_operations.h
+++ matrix_operations.h
@@ -1,6 +1,36 @@
 #ifndef MATRIX_OPERATIONS
 #define MATRIX_OPERATIONS
 
+#include <iostream>
+#include <iomanip>
+#include "matrix_operations.h"
+using namespace std;
+
+//создание нулевой квадратной матрицы
+double** create_zero_matrix(int n);
+
+// проверка корректности ввода
+template <class T>
+T read_and_check(T a);
+
+//ввод исходной квадратной матрицы (source_matrix)
+double** input_source_matrix(int n, const char *name_matrix);
+
+// создание квадратной матрицы
+double** creating_matrix(int n);
+
+//сложение матриц
+double** multiply_matrix_by_num(double** matrix, int num, int n);
+
+//умножение матрицы на число
+double** sum_matrix_with_matrix(double** matrix1, double** matrix2, int n);
+
+//сложение матрицы с числом
+double** sum_matrix_with_num(double** matrix, int num, int n);
+
+//умножение матриц
+double** multiply_matrix_by_matrix(double** matrix1, double** matrix2, int n);
+
 //нахождение строки с наибольшим кол-вом нулей
 int finding_row_with_max_nulls(double** source_matrix, int n);
 
@@ -10,37 +40,32 @@ void minor_matrix(double** matrix, double** source_matrix, int idx1, int idx2, i
 //вычисление определител€ квадратной матрицы
 double det_matrix(double** matrix, int row, int n);
 
-//создание нулевой квадратной матрицы
-double** create_zero_matrix(int n);
+//расчЄт функции f(x) = x^3 + 2x^2 + 5x + 10
+double** function_evaluation(double** source_matrix, int n, const char *name_matrix);
 
-//ввод исходной квадратной матрицы (source_matrix)
-double** input_source_matrix(int n);
+//вычисление матрицы A'
+double **new_matrix_a(double** matrix_a, int n);
 
-//умножение матриц
-double** multiply_matrix_by_matrix(double** matrix1, double** matrix2, int n);
+// получение матрицы без i-й строки и j-го столбца (т. е. перенумеровывание элементов)
+double** create_minor(double** matrix, int n, int row_idx, int column_idx);
 
-//сложение матриц
-double** sum_matrix_with_matrix(double** matrix1, double** matrix2, int n);
+// вычисление определител€ матрицы
+double determinant(double** matrix, int n);
 
-//умножение матрицы на число
-double** multiply_matrix_by_num(double** matrix, int num, int n);
+// создание обратной матрицы
+double** create_inverse_matrix(double** matrix, int n);
 
-//сложение матрицы с числом
-double** sum_matrix_with_num(double** matrix, int num, int n);
-
-//обратна€ матрица 2х2
-double** inverse_matrix(double** matrix, int n);
+//вычисление матрицы X
+double** evaluation_matrix_X(double** matrix_a, double** matrix_b, double** matrix_c, int n);
 
 //вывод матрицы
-void matrix_output(double** matrix, int n);
+void matrix_output(double** matrix, int n, const char *name_matrix);
+
+// итогова€ очистка пам€ти
+void end_clear_memory(double **res_matrix_a, double **res_matrix_b, double **matrix_a,
+	double **matrix_b, double **matrix_c, int n);
 
 //очистка динамически выделенной пам€ти
 void clear_memory(double** matrix, int n);
-
-//расчЄт функции f(x) = x^3 + 2x^2 + 5x + 10
-double** function_evaluation(double** source_matrix, int n);
-
-//вычисление матрицы ’ из уравнени€ јX+¬=—
-double** evaluation_matrix_X(double** matrix_a, double** matrix_b, double** matrix_c, int n);
 
 #endif MATRIX_OPERATIONS
